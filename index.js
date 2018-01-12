462128c985b5c767b99749501db711c3cdeb26e2
unknown <caterina.battisti@studenti.unitn.it>
2018-01-12 11:26:25 +0100

    heroku

--- index.js
+++ index.js
@@ -1,38 +1,30 @@
 const express = require('express')
 const path = require('path')
-const { spawn } = require('child_process');
 const request = require('request');
-const test = require('tape');
-const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+const area = require('./area')
 
-// Start the app
-const env = Object.assign({}, process.env, {PORT: 5000});
-const child = spawn('node', ['index.js'], {env});
+function areaJson(area){
+	json = "{";
+	json += "area: "
+	json += area;
+	json += "}";
+	return json;
+}
 
-test('responds to requests', (t) => {
-  t.plan(4);
+app.use('/area', area);
+app.set('port', (process.env.PORT || 5000));
+app.get('/getArea', function(req, res){
+	var area = area.getArea([req.query.side1, req.query.side2]);
+	if(area == -1){
+		res.status = 400;
+	} else {
+		res.status = 200;
+		res.json(areaJson(area));
+	}
+});
 
-  // Wait until the server is ready
-  child.stdout.on('data', _ => {
-    // Make a request to our app
-    request('http://127.0.0.1:5000', (error, response, body) => {
-      // stop the server
-      child.kill();
-
-      // No error
-      t.false(error);
-      // Successful response
-      t.equal(response.statusCode, 200);
-      // Assert content checks
-      t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-      t.notEqual(body.indexOf("Getting Started with Node on Heroku"), -1);
-    });
-  });
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });
